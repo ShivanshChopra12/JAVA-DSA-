417c4931f11f9849f36d438bf4c8ba59b92a7f14
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-09-27 23:18:30 +0530

    Create SearchInsertPosition.java

---
+++ BinarySearch/SearchInsertPosition.java
@@ -0,0 +1,25 @@
+package binarySearch;
+//https://leetcode.com/problems/search-insert-position/
+
+public class SearchInsertPosition {
+
+	public static void main(String[] args) {
+	}
+
+	static public int searchInsert(int[] nums, int target) {
+		int start = 0;
+		int end = nums.length - 1;
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			if (target < nums[mid]) {
+				end = mid - 1;
+			} else if (target > nums[mid]) {
+				start = mid + 1;
+			} else {
+				return mid;
+			}
+		}
+		return start;
+
+	}
+}
