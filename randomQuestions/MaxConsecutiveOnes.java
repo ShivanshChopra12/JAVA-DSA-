6001a6da5f0ddb9b5255dba8259e74af862098d0
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-10-11 23:35:09 +0530

    Create MaxConsecutiveOnes.java

---
+++ randomQuestions/MaxConsecutiveOnes.java
@@ -0,0 +1,25 @@
+package randomQuestions;
+
+public class MaxConsecutiveOnes {
+
+	public static void main(String[] args) {
+		int [] nums = {1,1,0,1,1,1,1};
+		System.out.println(findMaxConsecutiveOnes(nums));
+		
+	}
+
+	public static int findMaxConsecutiveOnes(int[] nums) {
+		int count = 0;
+		int ans = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 0) {
+				count = 0;
+			} else {
+				count++;
+			}
+			ans = Math.max(ans, count);
+		}
+		return ans;
+	}
+}
