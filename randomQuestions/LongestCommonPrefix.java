53a0738c463319375e0b8956a208ebf6af689736
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-10-19 03:45:10 +0530

    ques

--- randomQuestions/LongestCommonPrefix.java
+++ randomQuestions/LongestCommonPrefix.java
@@ -15,7 +15,8 @@ public class LongestCommonPrefix {
 		}
 		String prefix = strs[0]; // prefix = "flower"
 		for (int i = 1; i < strs.length; i++) {
-			while (strs[i].indexOf(prefix) != 0) { // i =1 , strs me "flow"
+			while (strs[i].indexOf(prefix) != 0) { // i =1 , strs me "flow" //flow.indexof flower != 0, condition true ,
+													// enter the loop
 				prefix = prefix.substring(0, prefix.length() - 1);
 				// 1st pass, flower - 1 = flowe //2nd pass flowe -1 = flow
 				// now i = 2 //1st pass = flow -1 = flo //2nd pass flo -1 = fl
---
+++ randomQuestions/ThreeSum.java
@@ -0,0 +1,36 @@
+package randomQuestions;
+
+import java.util.*;
+
+public class ThreeSum {
+
+	public static void main(String[] args) {
+		int arr[] = { -1, 0, 1, 2, -1, -4 };
+		System.out.println(threeSum(arr));
+	}
+
+	public static List<List<Integer>> threeSum(int[] nums) {
+		Set<List<Integer>> res = new HashSet<>(); // Sets list bcz no duplicates
+		if (nums.length == 0)
+			return new ArrayList<>(res);
+		Arrays.sort(nums); // sorted the array by default ascending
+		for (int i = 0; i < nums.length - 2; i++) {
+			int j = i + 1; // i at 0 ; j at i=1 and k at last
+			int k = nums.length - 1;
+			while (j < k) {
+				int sum = nums[j] + nums[k];
+				if (sum == -nums[i]) { // sum of k and j = - i then we can say i+j+k=0
+					res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+					j++;
+					k--;
+				} else if (sum > -nums[i]) // if sum is greater that means we have to reduce so k-- , bcz array is
+											// ascending , so k-- will reduce the sum
+					k--;
+				else if (sum < -nums[i]) // to inc the sum , inc j
+					j++;
+			}
+		}
+		return new ArrayList<>(res);
+	}
+
+}
