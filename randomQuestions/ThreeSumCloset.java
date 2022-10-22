2505fe5eb2bf78f186af34ac4061d27815028843
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-10-23 02:40:08 +0530

    Update ThreeSumCloset.java

--- randomQuestions/ThreeSumCloset.java
+++ randomQuestions/ThreeSumCloset.java
@@ -1,10 +1,39 @@
 package randomQuestions;
 
+//https://leetcode.com/problems/3sum-closest/submissions/
+import java.util.Arrays;
+
 public class ThreeSumCloset {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int nums[] = { -1, 2, 1, -4 };
+		int target = 1;
 
+		System.out.println(threeSumClosest(nums, target));
+	}
+
+	public static int threeSumClosest(int[] nums, int target) {
+		int res = Integer.MAX_VALUE;
+
+		Arrays.sort(nums);
+
+		for (int i = 0; i < nums.length - 2; i++) {
+			int j = i + 1;
+			int k = nums.length - 1;
+			while (j < k) {
+				int cur_sum = nums[i] + nums[j] + nums[k];
+				if (cur_sum > target) {
+					k--;
+				} else {
+					j++;
+				}
+
+				if (Math.abs(cur_sum - target) < Math.abs(res - target)) {
+					res = cur_sum;
+				}
+			}
+		}
+		return res;
 	}
 
 }
