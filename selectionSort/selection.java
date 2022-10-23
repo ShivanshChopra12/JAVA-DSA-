6e2cd130443c00c1f3044d45e067e4a8de770506
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-10-24 02:55:25 +0530

    Update selection.java

--- selectionSort/selection.java
+++ selectionSort/selection.java
@@ -1,15 +1,17 @@
 package selectionSort;
 
 import java.util.Arrays;
+
 public class selection {
 
 	public static void main(String[] args) {
-		int[] arr = { 3, 1, 5, 2, 4 };
+		int[] arr = { 3, 1, 5, 2, 4 }; // 3,1,4,2,5 //3,1,2,4,5 //2,1,3,4,5 //1,2,3,4,5
 		selectionSort(arr);
 		System.out.println(Arrays.toString(arr));
 	}
 
-	// find the maximum array in the remaining array and swap it with correct index
+	// find the maximum in array in the remaining array and swap it with correct
+	// index
 	static void selectionSort(int[] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			int last = arr.length - i - 1;
@@ -30,9 +32,9 @@ public class selection {
 	}
 
 	// swap the largest with its proper index
-	static void swap(int[] arr, int first, int second) {
-		int temp = arr[first];
-		arr[first] = arr[second];
+	static void swap(int[] arr, int maxIndex, int second) {
+		int temp = arr[maxIndex];
+		arr[maxIndex] = arr[second];
 		arr[second] = temp;
 	}
 }
