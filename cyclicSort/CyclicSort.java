388daa65fb1aa2f920e48a1a0c76aa582c21db2f
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-10-28 02:58:41 +0530

    Create CyclicSort.java

---
+++ cyclicSort/CyclicSort.java
@@ -0,0 +1,33 @@
+package cyclicSort;
+
+import java.util.Arrays;
+
+//when no.s are given from range 1 to N = use cyclic sort
+public class CyclicSort {
+	public static void main(String[] args) {
+		int arr[] = { 3, 5, 2, 1, 4 };
+		sort(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+
+	// 0th position pe 1, 1st position pe 2 ; means value at a given position must
+	// be on value -1 position
+	public static void sort(int[] arr) {
+		int i = 0;
+		while (i < arr.length) {
+			int correct = arr[i] - 1;
+			if (arr[i] != arr[correct]) {
+				swap(arr, i, correct);
+			} else {
+				i++;
+			}
+		}
+	}
+
+	public static void swap(int[] arr, int first, int second) {
+		int temp = arr[first];
+		arr[first] = arr[second];
+		arr[second] = temp;
+	}
+
+}
