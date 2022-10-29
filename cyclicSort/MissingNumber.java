83d3cb8537c975d1b6c76d6803934028697530e0
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-10-30 03:24:42 +0530

    Update MissingNumber.java

--- cyclicSort/MissingNumber.java
+++ cyclicSort/MissingNumber.java
@@ -6,6 +6,7 @@ public class MissingNumber {
 	public static void main(String[] args) {
 		int[] arr = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
 		System.out.println(missingnumber(arr));
+
 	}
 
 	public static int missingnumber(int[] arr) {
