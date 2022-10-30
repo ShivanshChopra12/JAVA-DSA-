4a1036c38c2c3709cefdc040add873f0f53b7361
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-10-30 23:58:05 +0530

    Update CyclicSort.java

--- cyclicSort/CyclicSort.java
+++ cyclicSort/CyclicSort.java
@@ -9,11 +9,10 @@ public class CyclicSort {
 		sort(arr);
 		System.out.println(Arrays.toString(arr));
 	}
-
 	// 0th position pe 1, 1st position pe 2 ; means value at a given position must
 	// be on value -1 position
 	public static void sort(int[] arr) {
-		int i = 0; 
+		int i = 0;
 		while (i < arr.length) {
 			int correct = arr[i] - 1;
 			if (arr[i] < arr.length && arr[i] != arr[correct]) {
