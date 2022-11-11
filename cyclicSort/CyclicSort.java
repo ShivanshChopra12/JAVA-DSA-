24dd737517a35f476f8f2a6ec009b817cf68092f
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-11-12 01:37:30 +0530

    Update CyclicSort.java

--- cyclicSort/CyclicSort.java
+++ cyclicSort/CyclicSort.java
@@ -17,7 +17,7 @@ public class CyclicSort {
 		while (i < arr.length) {
 			int correct = arr[i] - 1;
 			if (arr[i] < arr.length && arr[i] != arr[correct]) {
-				swap(arr, i, correct);
+				swap(arr, i, correct); //{2,5,3,1,4}, {5,2,3,1,4},{4,2,3,1,5},{1,2,3,4,5}
 			} else {
 				i++;
 			}
