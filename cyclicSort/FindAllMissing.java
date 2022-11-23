0489bb0a62b59235f8b21dd0bdb365ba199374ae
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-11-23 20:44:09 +0530

    Update FindAllMissing.java

--- cyclicSort/FindAllMissing.java
+++ cyclicSort/FindAllMissing.java
@@ -6,27 +6,27 @@ import java.util.List;
 //https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 
 public class FindAllMissing {
-	public List<Integer> findDisappearedNumbers(int[] nums) {
+	public List<Integer> findDuplicates(int[] nums) {
 		int i = 0;
 		while (i < nums.length) {
-			int correct = nums[i];
-			if (nums[i] < nums.length && nums[i] != nums[correct]) {
+			int correct = nums[i] - 1;
+			if (nums[i] != nums[correct]) {
 				swap(nums, i, correct);
 			} else {
 				i++;
 			}
 		}
 
-		// finding missing no.s
 		List<Integer> ans = new ArrayList<>();
 		for (int index = 0; index < nums.length; index++) {
 			if (nums[index] != index + 1) {
-				ans.add(index + 1);
+				ans.add(nums[index]);
 			}
 		}
 		return ans;
+
 	}
-	
+
 	static void swap(int[] arr, int first, int second) {
 		int temp = arr[first];
 		arr[first] = arr[second];
