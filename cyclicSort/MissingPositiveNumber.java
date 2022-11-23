0973fb88d0b9bf3c36f7e631c7860ea29c8b7929
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-11-24 02:22:13 +0530

    cyclic sort complete

--- cyclicSort/MissingPositiveNumber.java
+++ cyclicSort/MissingPositiveNumber.java
@@ -1,18 +1,16 @@
 package cyclicSort;
 
-//https://leetcode.com/problems/missing-number/
-public class MissingNumber {
+public class MissingPositiveNumber {
 
 	public static void main(String[] args) {
-		int[] arr = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
-		System.out.println(missingnumber(arr));
+
 	}
 
 	public static int missingnumber(int[] arr) {
 		int i = 0;
 		while (i < arr.length) {
-			int correct = arr[i];
-			if (arr[i] < arr.length && arr[i] != arr[correct]) {
+			int correct = arr[i] - 1;
+			if (arr[i] > 0 && arr[i] <= arr.length && arr[i] != arr[correct]) {
 				swap(arr, i, correct);
 			} else {
 				i++;
@@ -21,11 +19,11 @@ public class MissingNumber {
 
 		// searching for first missing element
 		for (int index = 0; index < arr.length; index++) {
-			if (arr[index] != index) {
-				return index;
+			if (arr[index] != index + 1) {
+				return index + 1;
 			}
 		}
-		return arr.length;
+		return arr.length + 1; // {1,2,3,4} so return 5 bcz 5 is the smallest positive missing no.
 	}
 
 	public static void swap(int[] arr, int first, int second) {
