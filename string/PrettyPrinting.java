5798e81429b3f3a42fd4cc2b4e12e4fcb1b57368
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-11-26 00:35:05 +0530

    Create PrettyPrinting.java

---
+++ string/PrettyPrinting.java
@@ -0,0 +1,13 @@
+package string;
+
+public class PrettyPrinting {
+
+	public static void main(String[] args) {
+		float a = 453.1274543f;
+
+		System.out.printf("Formatted no. is %.2f. \n", a);
+		
+		System.out.printf("My name is %s and i am an %s.", "Shivansh", "Engineer");
+	}
+
+}
