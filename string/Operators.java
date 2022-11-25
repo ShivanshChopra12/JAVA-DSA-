8cc29265ca6a0a07332ee8641aabe5fb9f245eee
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-11-26 00:38:48 +0530

    Update Operators.java

--- string/Operators.java
+++ string/Operators.java
@@ -5,9 +5,8 @@ public class Operators {
 	public static void main(String[] args) {
 		System.out.println('a' + 'b');
 		System.out.println("a" + "b");
-		System.out.println('a'+ 3);
-		System.out.println((char)('a'+ 3));
-
+		System.out.println('a' + 3);
+		System.out.println((char) ('a' + 3));
+		System.out.println("a" + 3);
 	}
-
 }
