602051877d2e52c5d697794fc933145d306745eb
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-11-26 00:38:11 +0530

    Create Operators.java

---
+++ string/Operators.java
@@ -0,0 +1,13 @@
+package string;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		System.out.println('a' + 'b');
+		System.out.println("a" + "b");
+		System.out.println('a'+ 3);
+		System.out.println((char)('a'+ 3));
+
+	}
+
+}
