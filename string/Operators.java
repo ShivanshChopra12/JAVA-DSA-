b3e2a7c14a14ea042d5b79d3da96af17af17e59d
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-11-26 00:44:39 +0530

    Update Operators.java

--- string/Operators.java
+++ string/Operators.java
@@ -9,5 +9,7 @@ public class Operators {
 		System.out.println((char) ('a' + 3));
 		System.out.println("a" + 3); // integer will be converted to Integer(Wrapper class)
 										// which calls toString()
+
+		System.out.println("Shivansh" + 3);
 	}
 }
