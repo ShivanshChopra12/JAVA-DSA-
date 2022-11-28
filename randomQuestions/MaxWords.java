cdb7a4ae39b009fecc9cb64b28cf2922444f87bc
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-11-29 00:18:04 +0530

    Update MaxWords.java

--- randomQuestions/MaxWords.java
+++ randomQuestions/MaxWords.java
@@ -1,12 +1,15 @@
 package randomQuestions;
 
+//https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/submissions/
 public class MaxWords {
 
 	public static void main(String[] args) {
 
+		String[] s = { "alice and bob love leetcode", "i think so too", "this is great thanks very much" };
+		System.out.println(mostWordsFound(s));
 	}
 
-	public int mostWordsFound(String[] sentences) {
+	public static int mostWordsFound(String[] sentences) {
 		int max = 0;
 		for (String s : sentences) {
 			int count = 1;
