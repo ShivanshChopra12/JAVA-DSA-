ff8b0a0d7d7353b29636f2418f24bfdda195ec51
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-12-10 22:46:52 +0530

    Create PowerOfTwo.java

---
+++ randomQuestions/PowerOfTwo.java
@@ -0,0 +1,22 @@
+package randomQuestions;
+
+//https://leetcode.com/problems/power-of-two/submissions/
+public class PowerOfTwo {
+	public static void main(String[] args) {
+		int a = 19;
+		System.out.println(isPowerOfTwo(a));
+	}
+
+	public static boolean isPowerOfTwo(int n) {
+		if (n < 1) {
+			return false;
+		}
+		if (n == 1) {
+			return true;
+		}
+		if (n % 2 == 1) {
+			return false;
+		}
+		return (isPowerOfTwo(n / 2));
+	}
+}
