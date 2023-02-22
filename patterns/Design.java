651ca64355ad527b8f409089b915122202323072
ShivanshChopra12 <shivichopra.sc@gmail.com>
2023-02-23 01:36:04 +0530

    Update Design.java

--- patterns/Design.java
+++ patterns/Design.java
@@ -4,8 +4,10 @@ public class Design {
 
 	public static void main(String[] args) {
 		pattern2(5);
+		pattern5(5);
 
 	}
+	
 	static void pattern2(int n) {
         for (int row = 1; row <= n; row++) {
             // for every row, run the col
@@ -15,5 +17,16 @@ public class Design {
             // when one row is printed, we need to add a newline
             System.out.println();
         }
-    }
+	}
+	
+	static void pattern5(int n) {
+        for (int row = 0; row < 2*n; row++) {
+            int TotalColInRow = row > n ? 2*n- row :row;
+            for (int col = 0; col <TotalColInRow ; col++) {
+                System.out.print("* ");
+            }
+            // when one row is printed, we need to add a newline
+            System.out.println();
+        }
+        }
 }
